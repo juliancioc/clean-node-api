8475062bbbc50932fcbd35ba1a736298d1cf0d6a
Juliancio Carvalho <juliancio.carvalho@fcamara.com.br>
2021-03-08 09:16:56 -0300

    refactor: ensure SIgnUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,17 +1,24 @@
+import { HttpRequest, HttpResponse } from "../protocols/http";
+
 export class SignUpController {
-    handle (httpRequest: any): any {
-        if(!httpRequest.body.name) {
-            return {
-                statusCode: 400,
-                body: new Error('Missing param: name')
-            }
-        }
-        if(!httpRequest.body.email) {
-            return {
-                statusCode: 400,
-                body: new Error('Missing param: email')
-            }
-        }
+  handle(httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: name"),
+      };
     }
-    
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: email"),
+      };
+    }
+
+    //solução temporária para erro na tipagem
+    return {
+        body: "an",
+        statusCode: 500
+    }
+  }
 }
